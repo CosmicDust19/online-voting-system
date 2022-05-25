54241c391a2c006b9ad2e521a390a53a78c90a66
CosmicDust19 <smh01.2019@gmail.com>
2022-05-25 12:46:51 +0300

    Completed

--- ovs-server/src/test/java/edu/estu/ovs/AdminControllerIntegrationTest.java
+++ ovs-server/src/test/java/edu/estu/ovs/AdminControllerIntegrationTest.java
@@ -13,7 +13,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@WebMvcTest(AdminController.class)
 class AdminControllerIntegrationTest {
 
     @Autowired
